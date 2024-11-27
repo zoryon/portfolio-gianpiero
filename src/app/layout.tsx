45d28fc0ce7126f8c2a1fad2a47e7ab061ee1798
zoryon <gioelespata@gmail.com>
2024-11-27 18:06:26 +0100

    modified: home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
