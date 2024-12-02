a3743c631b06de98a8600c4b2294eee6e2581b9f
zoryon <gioelespata@gmail.com>
2024-12-02 21:54:16 +0100

    fixes

--- src/app/videography/page.tsx
+++ src/app/videography/page.tsx
@@ -10,7 +10,7 @@ const VideographyPage = () => {
     return (
         <section className="w-full">
             {/* main video row to navigate trough videos */}
-            <header className="w-full flex justify-center pt-16">
+            <header className="w-full flex justify-center pt-8 md:pt-16">
                 <iframe
                     className="w-full max-w-[850px] h-[278px] sm:max-w-[600px] sm:h-[337px] md:max-w-[700px] md:h-[394px] lg:max-w-[850px] lg:h-[478px]"
                     src={`https://www.youtube.com/embed/${selectedVideoId}`}
@@ -24,7 +24,7 @@ const VideographyPage = () => {
             {/* grid of non-clickable videos */}
             <main
                 className="w-full grid grid-cols-1 md:grod-cols-2 lg:grid-cols-3
-                gap-6 lg:gap-12 mt-14 lg:mt-20 mb-14 px-8 md:px-44 lg:px-10"
+                gap-6 lg:gap-12 mt-14 lg:mt-20 mb-14 px-4 md:px-44 lg:px-10"
             >
                 {VIDEOS_ARRAY.map(id => {
                     return (
